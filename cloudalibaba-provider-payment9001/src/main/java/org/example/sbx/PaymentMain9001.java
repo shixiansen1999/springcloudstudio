0bfa41ccc9b8b982f0e844b318792333fec24ef3
sbx <26050364543@qq.com>
2023-07-07 11:21:17 +0800

    feat(cloudalibaba-provider-payment9001): 新建模块

---
+++ cloudalibaba-provider-payment9001/src/main/java/org/example/sbx/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package org.example.sbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author : sbx
+ * @Classname : PaymentMain9001
+ * @Description : TODO
+ * @Date : 2023/7/7 10:27
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
