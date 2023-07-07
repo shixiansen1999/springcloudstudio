d3498727e21976d2eae2a1d5c9d1fb80499fb48e
sbx <26050364543@qq.com>
2023-07-07 14:27:22 +0800

    feat(cloudalibaba-provider-payment9002): 新建模块

---
+++ cloudalibaba-provider-payment9002/src/main/java/org/example/sbx/PaymentMain9002.java
@@ -0,0 +1,19 @@
+package org.example.sbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author : sbx
+ * @Classname : PaymentMain9002
+ * @Description : TODO
+ * @Date : 2023/7/7 12:20
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/org/example/sbx/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package org.example.sbx.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : sbx
+ * @Classname : PaymentController
+ * @Description : TODO
+ * @Date : 2023/7/7 10:36
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    private String getPayment(@PathVariable Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
