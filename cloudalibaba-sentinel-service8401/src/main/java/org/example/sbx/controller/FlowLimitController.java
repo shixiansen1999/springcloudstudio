1f94e4fded85eb0c3f660893c12ab854ad657704
sbx <26050364543@qq.com>
2023-07-17 17:52:15 +0800

    feat(): 热点key限流

--- cloudalibaba-sentinel-service8401/src/main/java/org/example/sbx/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/org/example/sbx/controller/FlowLimitController.java
@@ -1,10 +1,12 @@
 package org.example.sbx.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.omg.CORBA.TIMEOUT;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -44,5 +46,17 @@ public class FlowLimitController {
         log.info("testE 测试异常比例");
         int age = 10 / 0;
         return "------testE 测试异常比例";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealHanlerTestHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        int i = 10/0;
+        return "------testHotKey";
+    }
+
+    public String dealHanlerTestHotKey(String p1, String p2, BlockException e) {
+        return "------dealHandlerTestHotKey";
     }
 }
